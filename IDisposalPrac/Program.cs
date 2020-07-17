88bd1a939eab38f8e44d3a37c4f4b0b673c5d9e6
LIMMIHEE <48482259+LIMMIHEE@users.noreply.github.com>
2020-07-17 09:51:23 +0900

    Elice.io / 31-5

--- IDisposalPrac/Program.cs
+++ IDisposalPrac/Program.cs
@@ -11,7 +11,23 @@ namespace IDisposalPrac
     {
         static void Main(string[] args)
         {
-            File.WriteAllText(@"D:\기타\test.txt", "문자열을 넣습ㄴㅣ다");
+            //File.WriteAllText(@"D:\기타\test.txt", "다른 문자열을 넣습ㄴㅣ다");
+            //Console.WriteLine(File.ReadAllText(@"D:\기타\test.txt"));
+            
+            //var file = @"D:기타\test.txt";
+            //Console.WriteLine(File.ReadAllText(file));
+
+            using (StreamWriter writer = new StreamWriter(@"D:\기타\test.txt"))
+            {
+                writer.WriteLine("eilce.io");
+
+                for(int i=1; i<=10; i++)
+                {
+                    writer.WriteLine("<반복문>안녕하세요 "+i+"트");
+                }
+
+            }
+            Console.WriteLine(File.ReadAllText(@"D:\기타\test.txt"));
         }
     }
 }
