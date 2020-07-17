b51b2494991675d821a78dcd8851f5b6c2c29b0b
LIMMIHEE <48482259+LIMMIHEE@users.noreply.github.com>
2020-07-17 09:35:27 +0900

    초기 커밋.

---
+++ IDisposalPrac/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDisposalPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText(@"D:\기타\test.txt", "문자열을 넣습ㄴㅣ다");
+        }
+    }
+}
